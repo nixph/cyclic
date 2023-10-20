72a7ddfb429bb9aca58c798a570951a050493999
Your Name <you@example.com>
2023-10-20 08:42:09 +0800

    b

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import os
+import uvicorn
 from dotenv import load_dotenv
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
 
+app = FastAPI()
 load_dotenv()
 
-# Read the variable from the environment (or .env file)
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
 bot_token = os.getenv('BOT_TOKEN')
 secret_token = os.getenv("SECRET_TOKEN")
 
 print(" MAIN PROGRAM INITIALIZED")
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8181)
